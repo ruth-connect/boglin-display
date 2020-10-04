5c69f3740236294a5bf35780361742c24fd1921b
ruth <ruth@DESKTOP-HOVL7PT>
2020-10-04 21:44:06 +0100

    Initial creation of Gotek display service.

---
+++ src/main/java/uk/me/ruthmills/boglindisplay/service/GotekDisplayService.java
@@ -0,0 +1,6 @@
+package uk.me.ruthmills.boglindisplay.service;
+
+public interface GotekDisplayService {
+
+	public void displayText(String text);
+}
---
+++ src/main/java/uk/me/ruthmills/boglindisplay/service/impl/GotekDisplayServiceImpl.java
@@ -0,0 +1,34 @@
+package uk.me.ruthmills.boglindisplay.service.impl;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import uk.me.ruthmills.boglindisplay.service.GotekDisplayService;
+
+@Service
+public class GotekDisplayServiceImpl implements GotekDisplayService {
+
+	private static final String GOTEK_DISPLAY_PATH = "/home/pi/GotekLEDC68/TM1651";
+
+	private static final Logger logger = LoggerFactory.getLogger(GotekDisplayServiceImpl.class);
+
+	@PostConstruct
+	public void initialise() {
+		displayText("YES");
+	}
+
+	@Override
+	public void displayText(String text) {
+		try {
+			ProcessBuilder processBuilder = new ProcessBuilder(GOTEK_DISPLAY_PATH, text);
+			processBuilder.redirectErrorStream(true);
+			Process process = processBuilder.start();
+			process.waitFor();
+		} catch (Exception ex) {
+			logger.error("Exception", ex);
+		}
+	}
+}
