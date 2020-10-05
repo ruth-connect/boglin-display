1982f89c8db4339082b1aac105ac202658e7a05f
ruth <ruth@DESKTOP-HOVL7PT>
2020-10-05 15:40:50 +0100

    MVP - make Boglin display the Big Bedroom filtered temperature.

--- src/main/java/uk/me/ruthmills/boglindisplay/polling/HomeAssistantStatePoller.java
+++ src/main/java/uk/me/ruthmills/boglindisplay/polling/HomeAssistantStatePoller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import uk.me.ruthmills.boglindisplay.service.GotekDisplayService;
 import uk.me.ruthmills.boglindisplay.service.HomeAssistantQueryService;
 
 @Component
@@ -14,12 +15,16 @@ public class HomeAssistantStatePoller {
 	@Autowired
 	private HomeAssistantQueryService homeAssistantQueryService;
 
+	@Autowired
+	private GotekDisplayService gotekDisplayService;
+
 	private final Logger logger = LoggerFactory.getLogger(HomeAssistantStatePoller.class);
 
 	@Scheduled(cron = "0 */1 * * * *")
 	public void tick() {
 		try {
-			homeAssistantQueryService.querySensor("sensor.big_bedroom_filtered_temperature");
+			String temperature = homeAssistantQueryService.querySensor("sensor.big_bedroom_filtered_temperature");
+			gotekDisplayService.displayText(temperature);
 		} catch (Exception ex) {
 			logger.error("Exception in poller thread", ex);
 		}
